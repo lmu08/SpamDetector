4da49613af28d1df04821e6b5f0e4d5b3210b84c
Thierry TAN <libris@hotmail.fr>
2018-05-28 14:29:26 +0200

    SVM ADA & multicore
    More algo & speed boost

--- R/spambase.r
+++ R/spambase.r
@@ -1,13 +1,19 @@
-packages <- c("caret", "randomForest", "MASS", "nnet", "e1071")
+packages <- c("caret", "randomForest", "MASS", "nnet", "e1071", "ada")
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages())))  
 }
+
+install.packages("doMC", repos="http://R-Forge.R-project.org")
 
 library(caret)
 library(randomForest) # randomForest method
 library(MASS) # linear discrimant analysis
 library(nnet) # neural network
-library(e1071)
+library(ada)
+library(e1071) #funcctions for class analysis
+# configure multicore
+library(doMC)
+registerDoMC(cores=4)
 spam_data = read.csv(file = "https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data", header=FALSE)
 summary(spam_data)
 colnames(spam_data)[58] = "spam"   # change label of last column for convenience
@@ -28,11 +34,6 @@ tr_ctrl = trainControl(method = "repeatedcv",number = 10,repeats = 10)
 
 
 #train the models
-forest_train = train(spam ~ ., 
-                     data=training_set, 
-                     method="rf",
-                     trControl=tr_ctrl)
-
 lda_train = train(spam ~ ., 
                   data=training_set, 
                   method="lda",
@@ -48,14 +49,34 @@ knn_train = train(spam ~ .,
                   method="knn",
                   trControl=tr_ctrl)
 
+forest_train = train(spam ~ ., 
+                     data=training_set, 
+                     method="rf",
+                     trControl=tr_ctrl)
+
+svm_train = train(spam ~ ., 
+                  data=training_set, 
+                  method="svmLinear",
+                  trControl=tr_ctrl)
+ada_train = train(spam ~ ., 
+                  data=training_set, 
+                  method="ada",
+                  trControl=tr_ctrl)
+
 
 pred_forest = predict(forest_train, testing_set[,-58])
 pred_lda = predict(lda_train, testing_set[,-58])
 pred_nnet = predict(nnet_train, testing_set[,-58])
 pred_knn = predict(knn_train, testing_set[,-58])
+pred_svm = predict(svm_train, testing_set[,-58])
+pred_ada = predict(ada_train, testing_set[,-58])
 
 confusion_forest = confusionMatrix(pred_forest,testing_set$spam)
-confusionMatrix(pred_lda,testing_set$spam)
-confusionMatrix(pred_nnet,testing_set$spam)
+confusion_lda = confusionMatrix(pred_lda,testing_set$spam)
+confusion_nnet = confusionMatrix(pred_nnet,testing_set$spam)
 confusion_knn = confusionMatrix(pred_knn,testing_set$spam)
+confusion_svm = confusionMatrix(pred_svm, testing_set$spam)
+confusion_ada = confusionMatrix(pred_ada, testing_set$spam)
+
+
 plot(confusion_knn$table)
